e0491ba3fb8c0719a3324a7e357c55aed307a421
Z3R0CH13F <abacadaeafag0@gmail.com>
2023-08-28 19:54:06 +0800

    v0.0.6

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const { sc } = require("./lib/select-color");
-const { st } = require("./lib/style-text");
-const { warning } = require("./lib/func");
+const { sc } = require("./lib/select-color"),
+  { st } = require("./lib/style-text"),
+  { warning } = require("./lib/func");
 
-const Mf = (x) => Math.floor(x);
-const isArr = (x) => Array.isArray(x);
-const rgb = (r, g, b) => `\x1b[38;2;${Mf(r)};${Mf(g)};${Mf(b)}m`; // text color
-const br = (r, g, b) => `\x1b[48;2;${Mf(r)};${Mf(g)};${Mf(b)}m`; // background color
+const Mf = (x) => Math.floor(x),
+  isArr = (x) => Array.isArray(x),
+  rgb = (r, g, b) => `\x1b[38;2;${Mf(r)};${Mf(g)};${Mf(b)}m`, // text color
+  br = (r, g, b) => `\x1b[48;2;${Mf(r)};${Mf(g)};${Mf(b)}m`; // background color
 
 global.style = (x) => {
   let text_style = "";
-  let text = Array.isArray(x.text) ? x.text.join("\x20") : x.text || "";
+  let text = isArr(x.text) ? x.text.join("\x20") : x.text || "";
   let color = rgb(
     ...(x.color && isArr(x.color) && x.color.length === 3
       ? x.color
@@ -21,13 +21,11 @@ global.style = (x) => {
       : sc(x.background) || [0, 0, 0])
   );
 
-  if (x.textStyle !== undefined) {
+  if (x.textStyle) {
     // combination 2 or more text style ( BAD ENGLISH )
-    if (Array.isArray(x.textStyle)) {
+    if (isArr(x.textStyle)) {
       text_style = x.textStyle.map(st).join("");
-    } else {
-      if (typeof x.textStyle === "string") text_style = st(x.textStyle);
-    }
+    } else if (typeof x.textStyle === "string") text_style = st(x.textStyle);
   }
 
   return `${background}${color}${text_style}${text}\x1b[0m`;
--- test.js
+++ test.js
@@ -1,26 +1,2 @@
-const styler = require("./").example();
-
-let i = 0,
-  n = 255;
-
-while (i < 10) {
-  i++;
-  const x = style({
-    text: "Hello World",
-    color: [Math.random() * n, Math.random() * n, Math.random() * n],
-    background: [Math.random() * n, Math.random() * n, Math.random() * n],
-    textStyle: ["bold", "underline"],
-  });
-
-  console.log(x);
-}
-
-console.log(
-  style({
-    text: Array("hello dunia", "Power!"),
-    color: "red",
-    textStyle: Array("inverse", "blink", "strike"),
-  })
-);
-
-console.log(warning("Warning!"));
+const styler = require("./");
+console.log(style({ text: "hello world", color: "fulvous" }));
