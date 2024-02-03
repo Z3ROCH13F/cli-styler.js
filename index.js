691399b1c807f8dea596c4350a1f881b42a2c58a
Z3R0CH13F <abacadaeafag0@gmail.com>
2024-02-03 15:21:46 +0800

    v0.4.0 smaller?

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const { style } = require("./lib/style");
-const { styleR } = require("./lib/styleR");
-const { styleP } = require("./lib/styleP");
+const style = require("./lib/style");
+const styleR = require("./lib/styleR");
+const styleP = require("./lib/styleP");
 
 Object.prototype.Log = function () {
   console.log(this.valueOf());
 };
 
-module.exports = {
-  style,
-  styleR,
-  styleP
-};
+module.exports = { style, styleR, styleP };
--- test/index.js
+++ test/index.js
@@ -1,16 +1,17 @@
 const { style, styleR, styleP } = require("../");
 
 style({
-    color: "green",
-    text: ["Hello World", "Hello World2"]
+  color: "green",
+  text: ["Hello World", "Hello World2"],
+  line: "nline"
 }).Log();
 
 styleP.cyan.bglol.bold.ul
-    .text("Name:")
-    ._10.text("cli-styler.js")
-    ._n2.bg("red")
-    .text("v0.3.5")
-    .end.Log();
+  .text("Name:")
+  ._10.text("cli-styler.js")
+  ._n2.bg("red")
+  .text("v0.3.5")
+  .end.Log();
 styleP.bold.banana.text("Hello world").end.Log();
 styleP.bold.color("#e23ad8").text("Hello world ", "2024").end.Log();
 styleP.bold.color([200, 200, 0]).bgblue.text("Hello world").end.Log();
